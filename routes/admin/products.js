6441cba91e5ad932441c31852f54c8577252de3c
DevHeris <badairoinioluwa578@gmail.com>
2024-01-23 05:34:47 +0200

    Implemented a way to save the  image to the products.json file

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,15 @@
+// External libraries
 const express = require("express");
+const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
-const router = express.Router();
-
 const { requireTitle, requirePrice } = require("./validators");
-const { validationResult } = require("express-validator");
+
+const router = express.Router();
+// upload is a middleware
+const upload = multer({ storage: multer.memoryStorage() });
 
 // Route to list all products
 router.get("/admin/products", (req, res) => {});
@@ -14,10 +19,20 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  res.send("Submitted");
-});
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+
+    await productsRepo.create({ title, price, image });
+
+    res.send("Submitted");
+  }
+);
 
 module.exports = router;
