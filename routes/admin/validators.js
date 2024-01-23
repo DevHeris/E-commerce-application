c963f357f4af8ed8348a24717b8ebcaa997d370a
DevHeris <badairoinioluwa578@gmail.com>
2024-01-23 03:45:11 +0200

    Added custom error messages to requireTitle and requirePrice

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -53,6 +53,13 @@ module.exports = {
         throw new Error("Invalid password");
       }
     }),
-  requireTitle: check("title").trim().isLength({ min: 4, max: 20 }),
-  requirePrice: check("price").trim().toFloat().isFloat({ min: 1 }),
+  requireTitle: check("title")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Must be between 4 and 20 characters"),
+  requirePrice: check("price")
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage("Must be a number greater than 1"),
 };
