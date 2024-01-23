ab0bc7fc51a117547bbb56c8b317e12beef52167
DevHeris <badairoinioluwa578@gmail.com>
2024-01-23 06:50:00 +0200

    new file "middlewares.js" for custom reuseable error handling middleware in all the POST requests routes

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const { handleErrors } = require("./middlewares");
 
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
@@ -22,14 +22,9 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ errors }));
-    }
-
-    const { email, password, passwordConfirm } = req.body;
+    const { email, password } = req.body;
 
     // Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password });
@@ -53,19 +48,12 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
-
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
-
     res.send("You are signed in!!!");
   }
 );
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,6 @@
 // External libraries
 const express = require("express");
-const { validationResult } = require("express-validator");
+const { handleErrors } = require("./middlewares");
 const multer = require("multer");
 
 const productsRepo = require("../../repositories/products");
@@ -24,12 +24,8 @@ router.post(
   // This particular order is very important (i.e the array shouldn't come before the upload(multer) middleware)
   upload.single("image"),
   [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-
-    if (!errors.isEmpty()) return res.send(productsNewTemplate({ errors }));
-
     const image = req.file.buffer.toString("base64");
     const { title, price } = req.body;
 
