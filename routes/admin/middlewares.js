c07e93521509c93f283888f1d25b7c55d09ea200
DevHeris <badairoinioluwa578@gmail.com>
2024-01-25 04:39:02 +0200

    fixed the handleErrors middleware

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -1,12 +1,18 @@
 const { validationResult } = require("express-validator");
 
 module.exports = {
-  handleErrors(templateFunction) {
-    return (req, res, next) => {
+  handleErrors(templateFunction, dataCallback) {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        return res.send(templateFunction({ errors }));
+        let data = {};
+
+        if (dataCallback) {
+          data = await dataCallback(req);
+        }
+
+        return res.send(templateFunction({ errors, ...data }));
       }
 
       next();
