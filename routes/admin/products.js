00d28ca83dcde59912e897dfada30c3e82156bf3
DevHeris <badairoinioluwa578@gmail.com>
2024-01-25 02:25:23 +0200

    requireAuth custom middleware and template Update

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,6 @@
 // External libraries
 const express = require("express");
-const { handleErrors } = require("./middlewares");
+const { handleErrors, requireAuth } = require("./middlewares");
 const multer = require("multer");
 
 const productsRepo = require("../../repositories/products");
@@ -13,18 +13,19 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 // Route to list all products
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
 
 // Route to create new product
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   "/admin/products/new",
+  requireAuth,
   // This particular order is very important (i.e the array shouldn't come before the upload(multer) middleware)
   upload.single("image"),
   [requireTitle, requirePrice],
